1ad414a2b3c5b2116663da9c461d88cd7c496586
Dmitry Shneider <dmitryshneider@gmail.com>
2024-08-22 17:50:44 +0600

    Добавлено управление движением игроков мышкой

--- src/components/Canvas/ui/Canvas.tsx
+++ src/components/Canvas/ui/Canvas.tsx
@@ -21,7 +21,32 @@ const drawPlayers = (context: CanvasRenderingContext2D, canvas: HTMLCanvasElemen
     })
 }
 
-const updatePlayer = (player: Player, canvas: HTMLCanvasElement) => {
+const updatePlayer = (player: Player, canvas: HTMLCanvasElement, mouseRef: React.MutableRefObject<{
+    x: number;
+    y: number;
+}>) => {
+
+    if(mouseRef !== null) {
+        if(
+            mouseRef.current.y < player.y && 
+            mouseRef.current.y > player.y - player.r && 
+            mouseRef.current.y < player.y + player.r && 
+            mouseRef.current.x < player.x + player.r && 
+            mouseRef.current.x > player.x - player.r
+        ){
+            player.dy = player.dy > 0 ? player.dy : -player.dy
+        }
+        if(
+            mouseRef.current.y > player.y && 
+            mouseRef.current.y > player.y - player.r && 
+            mouseRef.current.y < player.y + player.r && 
+            mouseRef.current.x < player.x + player.r && 
+            mouseRef.current.x > player.x - player.r
+        ){
+            player.dy = player.dy < 0 ? player.dy : -player.dy
+        }
+    }
+
     player.y += player.dy
     if (player.y - player.r < 0) {
         player.y = player.r
@@ -31,7 +56,7 @@ const updatePlayer = (player: Player, canvas: HTMLCanvasElement) => {
     }
 }
 
-const drawSpells = (context: CanvasRenderingContext2D, spells: Spell[][]) => {
+const drawSpells = (context: CanvasRenderingContext2D) => {
     spells.forEach(spellArray => {
         spellArray.forEach(spell => {
             context.beginPath();
@@ -43,13 +68,13 @@ const drawSpells = (context: CanvasRenderingContext2D, spells: Spell[][]) => {
     });
 };
 
-const fireSpell = (playerIndex: number, spells: Spell[][]) => {
+const fireSpell = (playerIndex: number) => {
     const player = players[playerIndex];
     const newSpell = new Spell(player.x + (player.r + 10) * index(player), player.y, 10, index(player), player.spellBg);
     spells[playerIndex].push(newSpell);
 }
 
-    const checkSpellsCollision = (player: Player, playerSpells: Spell[], canvas: HTMLCanvasElement) => {
+const checkSpellsCollision = (player: Player, playerSpells: Spell[], canvas: HTMLCanvasElement) => {
         for (let i = playerSpells.length - 1; i >= 0; i--) {
             const spell = playerSpells[i]
             if (spell.x > canvas.width || spell.x < 0) {
@@ -66,11 +91,14 @@ const fireSpell = (playerIndex: number, spells: Spell[][]) => {
                 console.log(`Попал в игрока ${players.indexOf(player) + 1}`);
             }
         }
-    }
+}
 
-    const update = (canvas: HTMLCanvasElement) => {
+const update = (canvas: HTMLCanvasElement, mouseRef: React.MutableRefObject<{
+    x: number;
+    y: number;
+}>) => {
         players.forEach(player => {
-            updatePlayer(player, canvas)
+            updatePlayer(player, canvas, mouseRef)
             const playerIndex = players.indexOf(player);
             let oponentIndex
             if(playerIndex > 0) {
@@ -80,45 +108,51 @@ const fireSpell = (playerIndex: number, spells: Spell[][]) => {
             }
             spells[playerIndex].forEach(spell => {
                 spell.x += spell.dx
-                spell.y += player.dy
             });
             checkSpellsCollision(players[oponentIndex], spells[playerIndex], canvas)
         })
-    }
+}
 
 
 
     const Canvas = () => {
 
         const canvasRef = useRef<HTMLCanvasElement | null>(null);
+        const mouseRef = useRef<{ x: number, y: number }>({ x: 0, y: 0 })
+        const handleMouse = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+            mouseRef.current.x = event.clientX
+            mouseRef.current.y = event.clientY
+        }
 
         useEffect(() => {
             if (canvasRef) {
                 const canvas = canvasRef.current;
                 const context = canvas?.getContext('2d');
+                
                 if (canvas && context) {
                     const animate = () => {
-                        update(canvas);
+                        update(canvas, mouseRef);
                         drawPlayers(context, canvas);
-                        drawSpells(context, spells);
+                        drawSpells(context);
                         requestAnimationFrame(animate)
                     }
 
                     animate()
                     players.forEach((player, i) => {
                         setInterval(() => {
-                            fireSpell(i, spells)
+                            fireSpell(i)
                         }, player.fireRate)
                     })
                 }
             }
-        }, [])
+        }, [mouseRef])
 
         return (
             <canvas
                 ref={canvasRef}
                 width={600}
                 height={400}
+                onMouseMove={e => handleMouse(e)}
                 style={{ border: '1px solid #333' }}
             />
         )
